45787a332fcca7e48d64c91730f1826d4fedcdc9
Rahul Srivastava <rahulsrivastavaips@gmail.com>
2026-04-27 22:26:51 +0530

    Configure Firebase in Flutter app and add google-services.json

--- voluntraq_frontend/lib/main.dart
+++ voluntraq_frontend/lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'utils/theme.dart';
 import 'screens/role_selector_screen.dart';
 import 'screens/volunteer_home_screen.dart';
 import 'services/session_manager.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const VoluntraQApp());
 }
 
